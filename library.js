eb9eeda115bd0600e058e46ce609cba9702efea9
rusrap3657 <t3sserak@proton.me>
2024-11-09 19:15:37 -0500

    started library

---
+++ library.js
@@ -0,0 +1,31 @@
+export const entryHall = {
+  name: "Entry Hall",
+  description:
+    "The entry hall of the haunted house. It looks like no one has been here in a long time. There have been reports of monsters killers and so much more.",
+  lookAround:
+    "You check your surroundings only to notice the level of dust and the only other door in the room leads to what you assume is the parlor. There is a small table with a lamp on teh side.",
+  checkLamp:
+    "You check the lamp and notice that even though the light is on, there is no cable going to the wall for electricity.",
+  checktable:
+    "You check the table and notice a small key taped to the bottom of the table.",
+  checkDoor:
+    "You check the door and notice that it is locked. You will need a key to open it.",
+  checkSmallKey:
+    "You check the small key, it seems to match locks that are used for doors.",
+};
+
+export const parlor = {
+  name: "Parlor",
+  description:
+    "The parlor is a large room with a fireplace and a few chairs. There is a door that leads to the kitchen.",
+  lookAround:
+    "You check your surroundings and notice that the room is very dusty. The fireplace has not been used in a long time and the chairs are covered in dust.",
+  checkFireplace:
+    "You check the fireplace and notice that there is a small key inside.",
+  checkChairs:
+    "You check the chairs and notice that there is a small key taped to the bottom of one of the chairs.",
+  checkDoor:
+    "You check the door and notice that it is locked. You will need a key to open it.",
+  checkSmallKey:
+    "You check the small key, it seems to match locks that are used for doors.",
+};
